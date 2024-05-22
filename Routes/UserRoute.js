644ea9724d415efc8b64926c8a4e3aa92cfdab85
Shruti Vadgama <125206895+Shrut2210@users.noreply.github.com>
2024-05-22 17:41:17 +0530

    done

---
+++ Routes/UserRoute.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../Model/User.js');
+
+router.post('/signup', async (req, res) => {
+    let { username, email, password  } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(422).json({ error: 'please fill the fields properly' })
+    }
+    try {
+        const userExist = await User.findOne({ email: email })
+
+        if (userExist) {
+            console.log('user exist!!!');
+            return res.status(422).json({ error: 'User already exists' });
+        }
+
+        let hashPassword = await bcrypt.hash(password, 12);
+        password = hashPassword;
+
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+
+        return res.status(200).send({ message: "user registered successfully" + User.email + " and password " + User.password });
+        
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    let { email, password } = req.body;
+
+    if( !email || !password ){
+        return res.status(422).json({message : 'please enter required details'})
+    }
+
+    const LoginUser = await User.findOne({email : email});
+
+    if(!LoginUser) {
+        return res.status(400).send({ message: 'Invalid details' });
+    }
+
+    const isMatch = await bcrypt.compare(password , LoginUser.password);
+
+    if(!isMatch){
+        return res.status(400).send({ message: 'Invalid details' });
+    }
+
+    return res.status(200).send({ message: "Found the required user with email" + User.email + " and password " + User.password });
+})
+
+router.get('/', async (req, res) => {
+    let user = await User.find();
+    return res.json(user);
+})
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({path : './.env'});
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.json());
+
+require('./db/connection.js');
+
+const User = require('./Routes/UserRoute.js');
+app.use('/user', User);
+
+app.listen(process.env.PORT, () => {
+    console.log('PORT running');
+})
